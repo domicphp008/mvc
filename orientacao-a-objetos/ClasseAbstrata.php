16f0cbe4d3de4bffeba86dc6cd3e86604e9c0a6e
Domicio Jose <testedomicio@hotmail.com>
2021-07-28 23:07:53 -0300

    modificação na  ClasseAbstrata

--- orientacao-a-objetos/ClasseAbstrata.php
+++ orientacao-a-objetos/ClasseAbstrata.php
@@ -1,3 +1,21 @@
 <?php
 
 // Classe abstrada nao pode ser instanciada, somente herdada
+// Models sao as classe para trabalhar com o banco de dados
+class Crud{
+
+    public function cadastrar(){
+        return 'Domicio,   Aula 11 Classe abstrata , Domicio,  Aula 12 Níveis de acesso ';
+         
+    }
+
+}
+
+class UserModel extends Crud{
+
+}
+
+//$crud = new Crud();
+$user = new UserModel();
+/*echo  $user->nome();  ==> Aqui dtem que mudar o (nome) por (cadastrar) senão dá erro*/
+echo  $user->cadastrar();
