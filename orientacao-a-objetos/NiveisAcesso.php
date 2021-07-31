954da57447310def670897e8179c5b9d2373ae39
Domicio Jose <testedomicio@hotmail.com>
2021-07-30 22:51:31 -0300

    Niveis Acesso ao Atributos e Metodos

---
+++ orientacao-a-objetos/NiveisAcesso.php
@@ -0,0 +1,27 @@
+<?php
+
+//Public - E o menos seguro e visível em todos os lugares.
+//Protected - Segurança média, visível na própria classe e na classe que a herdou
+//Private - Nivel maximo de segurança, visto somente dentro da própria classe
+
+
+abstract class Crud{
+    private function cadastrar(){
+        return ' cadastrar dentro do crud';
+    }
+
+    public function cadastrar2(){
+        return $this->cadastrar();
+    }
+
+}
+
+class UserModel extends Crud{
+    public function cadastrarUser(){
+        return $this->cadastrar();
+    }
+
+}
+
+$user = new UserModel();
+echo $user->cadastrar2();
